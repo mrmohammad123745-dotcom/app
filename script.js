637016a6a46b900f8286c843adee566026bec644
mrmohammad123745-dotcom <mrmohammad123745@gmail.com>
2026-09-09 20:16:33 -0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const button = document.querySelector(".btn-menu");
-    const footer = document.querySelector("#scroll5");
 
-    const observer = new IntersectionObserver(([entry]) => {
-        button.classList.toggle("hide", entry.isIntersecting);
-    });
-
-    observer.observe(footer);
-});
 const menuBtn = document.querySelector(".menu-icon");
 const menu = document.querySelector(".nav-list");
 
